a4bc5215b31ab31bd927e393facb225bbb486e80
fqzappador <qzappador@gmail.com>
2020-05-28 16:06:38 -0400

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,19 +10,35 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
-  constructor( private http: HttpClient  ) {
+  equipo: any[] = [];
 
-    // console.log('Servicio de infoPagina listo');
+  constructor( private http: HttpClient ) {
 
-    // Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-        .subscribe( (resp: InfoPagina) => {
-
-          this.cargada = true;
-          this.info = resp;
-
-          console.log(resp);
-        } );
+    this.cargarInfo();
+    this.cargarEquipo();
 
   }
+
+  private cargarInfo() {
+    // Leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe( (resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+    });
+  }
+
+  private cargarEquipo() {
+
+    // Leer el archivo JSON
+    this.http.get('https://zappador-sportfolio.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
+      // console.log(resp);
+    });
+
+    // this.equipo = resp;
+  }
+
 }
